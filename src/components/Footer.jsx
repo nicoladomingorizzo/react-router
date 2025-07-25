2107e86251f83e58829b3892c7aeb96a2e942abc
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-07-25 18:25:57 +0200

    Changed header and footer bg to black

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
     return (
         <footer>
-            <div>
+            <div className="bg-black text-white py-3">
                 <h3 className="text-center my-5">LINK UTILI</h3>
                 <div className="d-flex flex-wrap justify-content-around align-items-center my-5 mx-3 text-center">
 
@@ -10,24 +10,24 @@ export default function Footer() {
                         <li>
                             <h4>Chi Siamo</h4>
                         </li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Missione</a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Visione </a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Il nostro team</a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Storia</a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Missione</a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Visione </a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Il nostro team</a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Storia</a></li>
                     </ul>
                     <ul className="list-unstyled">
                         <li><h4>Policy</h4></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Privacy Policy </a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Cookie Policy</a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Termini di servizio</a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Politica di accessibilità </a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Privacy Policy </a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Cookie Policy</a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Termini di servizio</a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Politica di accessibilità </a></li>
                     </ul>
                     <ul className="list-unstyled">
                         <li><h4>Link Utili</h4></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Assistenza clienti</a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Lavora con noi</a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Blog aziendale </a></li>
-                        <li><a className=' link-underline link-underline-opacity-0 text-dark' href="#">Contatti </a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Assistenza clienti</a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Lavora con noi</a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Blog aziendale </a></li>
+                        <li><a className=' link-underline link-underline-opacity-0 text-white' href="#">Contatti </a></li>
                     </ul>
 
                 </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,20 @@ export default function Header() {
     return (
         <header>
             <h1 className="text-center mt-5 mb-3">AZIENDA CINESE CHE SFRUTTA I BAMBINI PER FARE VESTITI</h1>
-            <ul className="nav justify-content-around mt-5 mb-3">
-                <li className="nav-item">
-                    <NavLink className="nav-link link-underline link-underline-opacity-0 text-dark" to="/" aria-current="page">Homepage</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link link-underline link-underline-opacity-0 text-dark" to="/products">Prodotti</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link link-underline link-underline-opacity-0 text-dark" to="/about">Chi Siamo</NavLink>
-                </li>
-            </ul>
+            <div className="bg-black">
+
+                <ul className="nav justify-content-around mt-5 mb-3 py-5">
+                    <li className="nav-item">
+                        <NavLink className="nav-link link-underline link-underline-opacity-0 text-white" to="/" aria-current="page">Homepage</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link link-underline link-underline-opacity-0 text-white" to="/products">Prodotti</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink className="nav-link link-underline link-underline-opacity-0 text-white" to="/about">Chi Siamo</NavLink>
+                    </li>
+                </ul>
+            </div>
         </header>
 
     )
